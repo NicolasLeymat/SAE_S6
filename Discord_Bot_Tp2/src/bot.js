522f8cf9cd236daf246c002b63cf760a270d854a
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2024-02-23 21:38:05 +0100

    change folder names

--- Discord_Bot_Tp2/src/bot.js
+++ Discord_Bot_Tp2/src/bot.js
@@ -1,50 +1,25 @@
 require('dotenv').config();
 const token = process.env.token;
 const Discord = require('discord.js'),
-Events = require('discord.js'),
-{ REST, Routes } = require('discord.js'),
-{ SlashCommandBuilder } = require('discord.js'),
 Client = new Discord.Client({
     //L'intent 32767 nous permet d'avoir tout les intents
     intents: 32767
-    });
-Client.commands = [];
+    }),
+fs = require('fs');
+Client.commands = new Discord.Collection(),
+Client.commandArray =  [];
+Client.cooldowns = new Discord.Collection();
 
-const rest = new REST().setToken(token);
-const command = new SlashCommandBuilder()
-.setName('ping')
-.setDescription("return pong");
-Client.commands.push(command);
-(async () => {
-	try {
-		console.log(`Started refreshing ${Client.commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands("1205874900003455006", "1011773050716442704"),
-			{ body: Client.commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
-})();
-
-Client.once("ready", Client => {
-    console.log("Client pret");
-});
-
-Client.login(token);
-
-Client.on("messageCreate", async message =>{
-    if(message.author.bot || !message.content=="ping")return;
-    message.reply("connard");
-});
-
-Client.on("interactionCreate", async interaction =>{
-    if(interaction.commandName == "ping"){
-        interaction.reply({content : "pong", ephemeral:true});
+const functionFolders = fs.readdirSync('./src/functions');
+for (const folder of functionFolders) {
+    const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith('.js'));
+    for(const file of functionFiles) {
+        require(`./functions/${folder}/${file}`)(Client);
     }
-});
+}
+
+Client.handleEvents();
+Client.handleCommands();
+Client.login(token);
